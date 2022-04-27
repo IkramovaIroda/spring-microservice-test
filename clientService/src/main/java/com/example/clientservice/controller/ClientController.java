8ab691b85cb4765797bb7d4851984ed4fc72e0f7
JafarbekTurayev <jafarbek1997@gmail.com>
2022-04-27 16:31:19 +0500

    changed

--- clientService/src/main/java/com/example/clientservice/controller/ClientController.java
+++ clientService/src/main/java/com/example/clientservice/controller/ClientController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/client")
 @RequiredArgsConstructor
 public class ClientController {
     final ClientRepository clientRepository;
--- departmentService/src/main/java/com/example/departmentservice/controller/DepartmentController.java
+++ departmentService/src/main/java/com/example/departmentservice/controller/DepartmentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/department")
 @RequiredArgsConstructor
 public class DepartmentController {
     private final DepartmentService departmentService;
